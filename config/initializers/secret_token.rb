09c4b803b6cd19afa2028aa9c81b08e80196132a
chandru <dckap@dckap-desktop.(none)>
2012-03-21 09:25:11 +0530

    my first ruby on rails appication commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmaps::Application.config.secret_token = 'ae703e1b5ae072f9212f63c16297c1ebb9ee45dff0f1a32ebe12a87bab2555a1a5d93da0e6c24c7129b92573bb804d7c53f7f9fa6ca3b6ec1604e9412741c6c1'
