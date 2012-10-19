8e4ef9e3b463e8667bd94d94c39bc1383281f717
chandru <dckap@dckap-desktop.(none)>
2012-10-19 18:25:01 +0530

    cap files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "first_app"
+set :repository,  "ssh://git@github.com/chandru-1987/first_app.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :user, "chandru-1987"
+set :scm_passphrase, "mozhi1987" 
+ssh_options[:forward_agent] = true
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
